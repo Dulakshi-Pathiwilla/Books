3ba88f72001362424ed082ce90b2a5fe936a4cc9
Dulakshi-Pathiwilla <pathiwi-ps21043@stu.kln.ac.lk>
2025-12-26 10:07:23 +0530

    Done

--- src/main/java/com/example/LibraryManagement/LibraryManagementApplication.java
+++ src/main/java/com/example/LibraryManagement/LibraryManagementApplication.java
@@ -8,13 +8,13 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class LibraryManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibraryManagementApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagementApplication.class, args);
 
-	}
+    }
 
     @Bean
-    public ModelMapper modelMapper(){
+    public ModelMapper modelMapper() {
         return new ModelMapper();
     }
 
